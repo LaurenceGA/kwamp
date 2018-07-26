68c4edccd5d158260ab86f932882c40eeba3956b
Laurence Armstrong <laurence.armstrong@airnz.co.nz>
2018-07-26 22:00:32 +1200

    remove websocket session cookies

--- src/main/kotlin/co/nz/arm/app/App.kt
+++ src/main/kotlin/co/nz/arm/app/App.kt
@@ -3,14 +3,10 @@ package co.nz.arm.app
 import co.nz.arm.wamp.Router
 import io.ktor.application.*
 import io.ktor.features.*
-import io.ktor.http.cio.websocket.CloseReason
 import io.ktor.http.cio.websocket.Frame
-import io.ktor.http.cio.websocket.close
 import io.ktor.http.cio.websocket.readText
 import io.ktor.response.respondText
 import io.ktor.routing.*
-import io.ktor.sessions.*
-import io.ktor.util.nextNonce
 import io.ktor.websocket.WebSockets
 import io.ktor.websocket.webSocket
 import kotlinx.coroutines.experimental.channels.consumeEach
@@ -26,34 +22,16 @@ fun Application.main() {
     }
 
     routing {
-        install(Sessions) {
-            cookie<SocketSession>("SESSION")
-        }
-
-        intercept(ApplicationCallPipeline.Infrastructure) {
-            if (call.sessions.get<SocketSession>() == null) {
-                call.sessions.set(SocketSession(nextNonce()))
-            }
-        }
-
         webSocket("/register") {
-            val session = call.sessions.get<SocketSession>()
-            if (session== null) {
-                close(CloseReason(CloseReason.Codes.VIOLATED_POLICY, "No session"))
-                return@webSocket
-            }
-
-            log.info("session=${session.id}")
-
             try {
-                incoming.consumeEach {frame ->
+                incoming.consumeEach { frame ->
                     if (frame is Frame.Text) {
                         log.info(frame.readText())
                         router.consume(frame)
                     }
                 }
             } finally {
-                log.info("Disconnected (${session.id})")
+                log.info("Disconnected")
             }
         }
 
