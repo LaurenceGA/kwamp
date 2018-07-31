95ec089b3d2b065efb5611b6dcbec4822db51241
Laurence Armstrong <laurence.armstrong@airnz.co.nz>
2018-07-31 22:12:28 +1200

    Tidied up toString for messages

--- src/main/kotlin/co/nz/arm/wamp/messages/Messages.kt
+++ src/main/kotlin/co/nz/arm/wamp/messages/Messages.kt
@@ -3,20 +3,20 @@ package co.nz.arm.wamp.messages
 import co.nz.arm.wamp.URI
 
 sealed class Message(val messageType: MessageType) {
-    override fun toString() = "[$messageType]"
+    override fun toString() = "[${messageType.id}]"
 }
 
 class Hello(realm: String, val details: Any) : Message(MessageType.HELLO) {
     val realm = URI(realm)
-    override fun toString() = "[$messageType, $realm, $details]"
+    override fun toString() = "[${messageType.id}, $realm, $details]"
 }
 
 class Welcome(val session: Int, val details: Any) : Message(MessageType.WELCOME) {
-    override fun toString() = "[$messageType, $session, $details]"
+    override fun toString() = "[${messageType.id}, $session, $details]"
 }
 
 class Abort(val details: Any, reason: String) : Message(MessageType.ABORT) {
     val reason = URI(reason)
 
-    override fun toString() = "[$messageType, $details, $reason]"
+    override fun toString() = "[${messageType.id}, $details, $reason]"
 }
