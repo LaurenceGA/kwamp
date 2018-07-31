24f219d9d82d4e46d34c0d947a56ec4708e04c01
Laurence Armstrong <laurence.armstrong@airnz.co.nz>
2018-07-31 22:12:41 +1200

    Fixed message serialisation validation

--- src/main/kotlin/co/nz/arm/wamp/messages/MessageFactory.kt
+++ src/main/kotlin/co/nz/arm/wamp/messages/MessageFactory.kt
@@ -5,17 +5,20 @@ import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.primaryConstructor
 import kotlin.reflect.jvm.jvmErasure
 
-inline fun <reified messageClass: Message> getFactory(): (objectArray: List<Any>) -> Message = {
+inline fun <reified messageClass : Message> getFactory(): (objectArray: List<Any>) -> Message = {
     validateArray(it, messageClass::class.primaryConstructor!!.parameters)
     messageClass::class::primaryConstructor.get()!!.call(*it.toTypedArray())
 }
 
 fun validateArray(objectArray: List<Any>, constructorParameters: List<KParameter>) {
-    if (objectArray.size != constructorParameters.size || canApplyValuesToParameters(objectArray, constructorParameters))
+    if (!areValidParameterValues(objectArray, constructorParameters))
         throw RuntimeException("Invalid message")
 }
 
-private fun canApplyValuesToParameters(values: List<Any>, parameters: List<KParameter>) = values.indices.none { i ->
+private fun areValidParameterValues(objectArray: List<Any>, constructorParameters: List<KParameter>) =
+        objectArray.size == constructorParameters.size && canApplyValuesToParameters(objectArray, constructorParameters)
+
+private fun canApplyValuesToParameters(values: List<Any>, parameters: List<KParameter>) = values.indices.all { i ->
     values[i].canBeAppliedToType(parameters[i])
 }
 
