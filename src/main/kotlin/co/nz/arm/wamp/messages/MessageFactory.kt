5546a3f7b8b3191d0118fcfef6760a34626d8ab1
Laurence Armstrong <laurence.armstrong@airnz.co.nz>
2018-08-08 13:32:29 +1200

    Converts message array objects into map to pass to constructor call

--- src/main/kotlin/co/nz/arm/wamp/messages/MessageFactory.kt
+++ src/main/kotlin/co/nz/arm/wamp/messages/MessageFactory.kt
@@ -9,20 +9,23 @@ import kotlin.reflect.jvm.jvmErasure
 
 fun generateFactory(messageClass: KClass<out Message>): (objectArray: List<Any>) -> Message = { inputList ->
     val parameters = messageClass.primaryConstructor!!.parameters
-    val mappedInputList = if (inputList.size in acceptableNumberOfParameters(parameters))
-        conformArrayObjectsToConstructor(inputList, parameters)
-    else
-        throw RuntimeException("Not enough parameters in message")
+    val mappedInputList = mapInputListTypesToParameters(inputList, parameters)
 
-    messageClass::primaryConstructor.get()!!.call(*mappedInputList.toTypedArray())
+    messageClass::primaryConstructor.get()!!.callBy(parameters.zip(mappedInputList).toMap())
 }
 
 private fun acceptableNumberOfParameters(parameters: List<KParameter>) = numberOfNonOptional(parameters)..parameters.size
 
 private fun numberOfNonOptional(parameters: List<KParameter>) = parameters.count { !it.isOptional }
 
+private fun mapInputListTypesToParameters(inputList: List<Any>, parameters: List<KParameter>) =
+        if (inputList.size in acceptableNumberOfParameters(parameters))
+            conformArrayObjectsToConstructor(inputList, parameters)
+        else
+            throw IllegalArgumentException("Not enough parameters in message")
+
 private fun conformArrayObjectsToConstructor(inputArray: List<Any>, parameters: List<KParameter>) = inputArray.mapIndexed { index, item ->
-    if (inputArray[index].canBeAppliedToType(parameters[index]) || inputArray[index] is Int)
+    if (inputArray[index].canBeAppliedToType(parameters[index]))
         item
     else
         getUnaryParameterConstructor(item, parameters[index]).call(item)
@@ -35,7 +38,7 @@ private fun canApplyValuesToParameters(values: List<Any>, parameters: List<KPara
 private fun getUnaryParameterConstructor(input: Any, parameter: KParameter): KFunction<Any> = try {
     parameter.type.jvmErasure.constructors.first { areValidParameterValues(listOf(input), it.parameters) }
 } catch (e: NoSuchElementException) {
-    throw RuntimeException("Type mismatch in message")
+    throw IllegalArgumentException("Couldn't find a way to create ${parameter.type.jvmErasure.simpleName} from $input")
 }
 
 private fun areValidParameterValues(objectArray: List<Any>, constructorParameters: List<KParameter>) =
