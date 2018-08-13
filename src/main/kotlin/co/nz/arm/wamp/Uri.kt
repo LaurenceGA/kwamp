2d81dc2f600498c1c2881c98ccd66a1ac4ea29d4
Laurence Armstrong <laurence.armstrong@airnz.co.nz>
2018-08-13 20:05:33 +1200

    Made Uri converter not companion anymore

--- src/main/kotlin/co/nz/arm/wamp/Uri.kt
+++ src/main/kotlin/co/nz/arm/wamp/Uri.kt
@@ -4,7 +4,7 @@ import com.beust.klaxon.Converter
 import com.beust.klaxon.JsonValue
 
 data class Uri(val uri: String) {
-    companion object UriConverter : Converter {
+    object UriConverter : Converter {
         override fun canConvert(cls: Class<*>) = cls == Uri::class.java
         override fun fromJson(jv: JsonValue): Any = false
         override fun toJson(value: Any) = "\"${(value as Uri).uri}\""
