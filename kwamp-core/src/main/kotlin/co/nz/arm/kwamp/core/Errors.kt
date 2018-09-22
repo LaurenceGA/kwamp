8ca42e4235bb15f3842bc7c8b236d96486316f5c
Laurence Armstrong <laurence.armstrong@airnz.co.nz>
2018-09-22 17:03:54 +1200

    changed error where uri 'wamp' was changed to 'kwamp'

--- kwamp-core/src/main/kotlin/co/nz/arm/kwamp/core/Errors.kt
+++ kwamp-core/src/main/kotlin/co/nz/arm/kwamp/core/Errors.kt
@@ -2,37 +2,37 @@ package co.nz.arm.kwamp.core
 
 enum class WampError(uri: String) {
     // Peer provided an incorrect URI for any URI-based attribute of WAMP message, such as realm, topic or procedure
-    INVALID_URI("kwamp.error.invalid_uri"),
+    INVALID_URI("wamp.error.invalid_uri"),
     // A Dealer could not perform a call, since no procedure is currently registered under the given URI.
-    NO_SUCH_PROCEDURE("kwamp.error.no_such_procedure"),
+    NO_SUCH_PROCEDURE("wamp.error.no_such_procedure"),
     // A procedure could not be registered, since a procedure with the given URI is already registered.
-    PROCEDURE_ALREADY_EXISTS("kwamp.error.procedure_already_exists"),
+    PROCEDURE_ALREADY_EXISTS("wamp.error.procedure_already_exists"),
     // A Dealer could not perform an unregister, since the given registration is not active.
-    NO_SUCH_REGISTRATION("kwamp.error.no_such_registration"),
+    NO_SUCH_REGISTRATION("wamp.error.no_such_registration"),
     // A Broker could not perform an unsubscribe, since the given subscription is not active.
-    NO_SUCH_SUBSCRIPTION("kwamp.error.no_such_subscription"),
+    NO_SUCH_SUBSCRIPTION("wamp.error.no_such_subscription"),
     // A call failed since the given argument types or values are not acceptable to the called procedure.
-    INVALID_ARGUMENT("kwamp.error.invalid_argument"),
+    INVALID_ARGUMENT("wamp.error.invalid_argument"),
     // A Peer received invalid WAMP protocol message (e.g. HELLO message after session was already established) - used as a ABORT reply reason.
-    PROTOCOL_VIOLATION("kwamp.error.protocol_violation"),
+    PROTOCOL_VIOLATION("wamp.error.protocol_violation"),
     // A join, call, register, publish or subscribe failed, since the Peer is not authorized to perform the operation.
-    NOT_AUTHORIZED("kwamp.error.not_authorized"),
+    NOT_AUTHORIZED("wamp.error.not_authorized"),
     // A Dealer or Broker could not determine if the Peer is authorized to perform a join, call, register, publish or subscribe, since the authorization operation itself failed. E.g. a custom authorizer did run into an error.
-    AUTHORIZATION_FAILED("kwamp.error.authorization_failed"),
+    AUTHORIZATION_FAILED("wamp.error.authorization_failed"),
     // Peer wanted to join a non-existing realm (and the Router did not allow to auto-create the realm).
-    NO_SUCH_REALM("kwamp.error.no_such_realm"),
+    NO_SUCH_REALM("wamp.error.no_such_realm"),
     // A Peer was to be authenticated under a Role that does not (or no longer) exists on the Router. For example, the Peer was successfully authenticated, but the Role configured does not exists - hence there is some misconfiguration in the Router.
-    NO_SUCH_ROLE("kwamp.error.no_such_role"),
+    NO_SUCH_ROLE("wamp.error.no_such_role"),
     // A Dealer or Callee canceled a call previously issued
-    CANCELED("kwamp.error.canceled"),
+    CANCELED("wamp.error.canceled"),
     // A Peer requested an interaction with an option that was disallowed by the Router
-    OPTION_NOT_ALLOWED("kwamp.error.option_not_allowed"),
+    OPTION_NOT_ALLOWED("wamp.error.option_not_allowed"),
     // A Dealer could not perform a call, since a procedure with the given URI is registered, but Callee Black- and Whitelisting and/or Caller Exclusion lead to the exclusion of (any) Callee providing the procedure.
-    NO_ELIGIBLE_CALLEE("kwamp.error.no_eligible_callee"),
+    NO_ELIGIBLE_CALLEE("wamp.error.no_eligible_callee"),
     // A Router rejected client request to disclose its identity
-    OPTION_DISALLOWED_DISCLOSE_ME("kwamp.error.option_disallowed.disclose_me"),
+    OPTION_DISALLOWED_DISCLOSE_ME("wamp.error.option_disallowed.disclose_me"),
     // A Router encountered a network failure
-    NETWORK_FAILURE("kwamp.error.network_failure");
+    NETWORK_FAILURE("wamp.error.network_failure");
 
 
     val uri = Uri(uri)
