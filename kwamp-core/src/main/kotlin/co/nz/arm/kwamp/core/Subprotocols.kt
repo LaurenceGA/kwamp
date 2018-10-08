ea8b8a5740681a050b3b65628e9aace36dbb8cb9
Laurence Armstrong <laurence.armstrong@airnz.co.nz>
2018-10-08 22:14:35 +1300

    fix subprotocol string

--- kwamp-core/src/main/kotlin/co/nz/arm/kwamp/core/Subprotocols.kt
+++ kwamp-core/src/main/kotlin/co/nz/arm/kwamp/core/Subprotocols.kt
@@ -1,5 +1,5 @@
 package co.nz.arm.kwamp.core
 
-const val WAMP_JSON: String = "kwamp.2.json"
-const val WAMP_MSG_PACK: String = "kwamp.2.msgpack"
+const val WAMP_JSON: String = "wamp.2.json"
+const val WAMP_MSG_PACK: String = "wamp.2.msgpack"
 const val WAMP_DEFAULT = WAMP_JSON
