7edcb548058f73b72d7cb89c5df6de23d145a1ce
Laurence Armstrong <laurence.armstrong@airnz.co.nz>
2018-10-10 21:06:18 +1300

    Surface RPC errors to client caller

--- kwamp-core/src/main/kotlin/co/nz/arm/kwamp/core/Uri.kt
+++ kwamp-core/src/main/kotlin/co/nz/arm/kwamp/core/Uri.kt
@@ -8,18 +8,18 @@ import com.squareup.moshi.JsonWriter
 import com.squareup.moshi.ToJson
 
 //TODO validate construction (S5.1.1) and return WampError.INVALID_URI on failure
-data class Uri(val uri: String) {
+data class Uri(val text: String) {
     object UriConverter : Converter {
         override fun canConvert(cls: Class<*>) = cls == Uri::class.java
         override fun fromJson(jv: JsonValue): Any = false
-        override fun toJson(value: Any) = "\"${(value as Uri).uri}\""
+        override fun toJson(value: Any) = "\"${(value as Uri).text}\""
     }
 
     object UriJsonAdapter : JsonAdapter<Uri>() {
         override fun fromJson(reader: JsonReader?): Uri? = null
         @ToJson
         override fun toJson(writer: JsonWriter?, uri: Uri?) {
-            writer!!.value(uri!!.uri)
+            writer!!.value(uri!!.text)
         }
     }
 }
