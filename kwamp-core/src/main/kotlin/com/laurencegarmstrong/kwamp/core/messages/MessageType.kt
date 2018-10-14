8ae9efc3101bf8059aae2ed1e36cb75754a54a0b
Laurence Armstrong <laurence.armstrong@airnz.co.nz>
2018-10-14 16:33:17 +1300

    removed repetition of factory generation and moved to more logical place

--- kwamp-core/src/main/kotlin/com/laurencegarmstrong/kwamp/core/messages/MessageType.kt
+++ kwamp-core/src/main/kotlin/com/laurencegarmstrong/kwamp/core/messages/MessageType.kt
@@ -1,35 +1,39 @@
 package com.laurencegarmstrong.kwamp.core.messages
 
 import com.laurencegarmstrong.kwamp.core.InvalidMessageException
+import kotlin.reflect.KClass
 
-enum class MessageType(val id: Int, val factory: (List<Any>) -> Message) {
-    HELLO(1, generateFactory(Hello::class)),
-    WELCOME(2, generateFactory(Welcome::class)),
-    ABORT(3, generateFactory(Abort::class)),
-    GOODBYE(6, generateFactory(Goodbye::class)),
-    ERROR(8, generateFactory(Error::class)),
-    PUBLISH(16, generateFactory(Publish::class)),
-    PUBLISHED(17, generateFactory(Published::class)),
-    SUBSCRIBE(32, generateFactory(Subscribe::class)),
-    SUBSCRIBED(33, generateFactory(Subscribed::class)),
-    UNSUBSCRIBE(34, generateFactory(Unsubscribe::class)),
-    UNSUBSCRIBED(35, generateFactory(Unsubscribed::class)),
-    EVENT(36, generateFactory(Event::class)),
-    CALL(48, generateFactory(Call::class)),
-    RESULT(50, generateFactory(Result::class)),
-    REGISTER(64, generateFactory(Register::class)),
-    REGISTERED(65, generateFactory(Registered::class)),
-    UNREGISTER(66, generateFactory(Unregister::class)),
-    UNREGISTERED(67, generateFactory(Unregistered::class)),
-    INVOCATION(68, generateFactory(Invocation::class)),
-    YIELD(70, generateFactory(Yield::class));
+enum class MessageType(val id: Int, val messageClass: KClass<out Message>) {
+    HELLO(1, Hello::class),
+    WELCOME(2, Welcome::class),
+    ABORT(3, Abort::class),
+    GOODBYE(6, Goodbye::class),
+    ERROR(8, Error::class),
+    PUBLISH(16, Publish::class),
+    PUBLISHED(17, Published::class),
+    SUBSCRIBE(32, Subscribe::class),
+    SUBSCRIBED(33, Subscribed::class),
+    UNSUBSCRIBE(34, Unsubscribe::class),
+    UNSUBSCRIBED(35, Unsubscribed::class),
+    EVENT(36, Event::class),
+    CALL(48, Call::class),
+    RESULT(50, Result::class),
+    REGISTER(64, Register::class),
+    REGISTERED(65, Registered::class),
+    UNREGISTER(66, Unregister::class),
+    UNREGISTERED(67, Unregistered::class),
+    INVOCATION(68, Invocation::class),
+    YIELD(70, Yield::class);
 
     companion object {
         private val factories = hashMapOf(*MessageType.values().map(Companion::toIndexedFactory).toTypedArray())
         private val messageTypes = hashMapOf(*MessageType.values().map(Companion::toIndexedIdentity).toTypedArray())
 
-        private fun toIndexedFactory(messageType: MessageType) = Pair(messageType.id, messageType.factory)
-        private fun toIndexedIdentity(messageType: MessageType) = Pair(messageType.id, messageType)
+        private fun toIndexedFactory(messageType: MessageType) =
+            Pair(messageType.id, generateFactory(messageType.messageClass))
+
+        private fun toIndexedIdentity(messageType: MessageType) =
+            Pair(messageType.id, messageType)
 
         fun getFactory(id: Int) = factories[id]
             ?: throw InvalidMessageException("Unknown message type '$id'")
