7b3f3eba708001151753dd9d2b0b81c4b49b614c
Laurence Armstrong <laurence.armstrong@airnz.co.nz>
2018-11-04 14:22:58 +1300

    Remove todo that's no longer applicable

--- kwamp-client-core/build.gradle.kts
+++ kwamp-client-core/build.gradle.kts
@@ -1,10 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val logbackVersion = "1.2.1"
-
 dependencies {
     compile(project(":kwamp-core"))
-
-    //TODO move logging to root project dependency
-    implementation("ch.qos.logback:logback-classic:$logbackVersion")
 }
