19550dad94d53e1b12acd7398940988032b94729
Laurence Armstrong <laurence.armstrong@airnz.co.nz>
2018-12-17 20:54:08 +1300

    publishing jars

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import groovy.lang.Closure
+import org.gradle.internal.impldep.org.apache.ivy.core.module.descriptor.Artifact
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -17,6 +19,8 @@ buildscript {
 plugins {
     base
     java
+//    maven
+    `maven-publish`
 }
 
 val kotlinVersion: String by extra
@@ -29,8 +33,8 @@ subprojects {
         plugin<KotlinPluginWrapper>()
     }
 
-    group = "com.laurencegarmstrong.kwamp"
-    version = "1.0.0"
+//    group = "com.laurencegarmstrong.kwamp"
+    group = "com.github.LaurenceGA"
 
     repositories {
         mavenCentral()
@@ -55,3 +59,109 @@ subprojects {
         testImplementation("io.kotlintest:kotlintest-runner-junit5:$kotlinTestVersion")
     }
 }
+
+task("artifacts") {
+    doLast {
+        val allArtifacts = configurations.archives.allArtifacts
+        println("Project has ${allArtifacts.size} artifacts:")
+        allArtifacts.forEach { artifact ->
+            println("'${artifact.name}'")
+        }
+    }
+}
+
+val kwampRouter = project(":kwamp-router-core").tasks["jar"]
+val kwampClient = project(":kwamp-client-core").tasks["jar"]
+
+publishing {
+    publications {
+        create<MavenPublication>("router") {
+            val project = project(":kwamp-router-core")
+            groupId = project.group as String
+            artifactId = project.properties["artifact_id"] as String
+            version = project.version as String
+
+            from(project.components["java"])
+        }
+    }
+    publications {
+        create<MavenPublication>("client") {
+            val project = project(":kwamp-client-core")
+            groupId = project.group as String
+            artifactId = project.properties["artifact_id"] as String
+            version = project.version as String
+
+            from(project.components["java"])
+        }
+    }
+}
+
+//artifacts {
+//    add("archives", kwampRouter) {
+//        type = "jar"
+//        name = "kwamp-router-core"
+//        group = project("kwamp-router-core").group
+//        version = project("kwamp-router-core").version
+//        classifier = project("kwamp-router-core").properties["classifier"] as String
+//        builtBy(kwampRouter)
+//    }
+//    add("archives", kwampClient) {
+//        type = "jar"
+//        name = "kwamp-client-core"
+//        group = project("kwamp-client-core").group
+//        version = project("kwamp-client-core").version
+//        classifier = project("kwamp-client-core").properties["classifier"] as String
+//        builtBy(kwampClient)
+//    }
+//}
+//
+//// We don't want the artifact produced by the root project
+//configurations.archives.allArtifacts.removeIf { artifact ->
+//    artifact.name == "kwamp"
+//}
+
+//val filter = object : PublishFilter {
+//    override fun accept(p0: Artifact?, p1: File?): Boolean {
+//        return true
+//    }
+//}
+
+//tasks.getByName<Upload>("install") {
+//    repositories.withGroovyBuilder {
+//        "mavenInstaller" {
+//            "addFilter"("test", filter)
+//        }
+//    }
+//    val installer = repositories["mavenInstaller"] as MavenResolver
+//    installer.addFilter("router") { artifact, _ ->
+//        artifact.name == "kwamp-router-core"
+//    }
+
+//    installer.addFilter("client") { artifact, _ ->
+//        artifact.name == "kwamp-client-core"
+//    }
+//    repositories.withGroovyBuilder {
+//        val installer = getProperty("mavenInstaller") as MavenResolver
+//        installer.addFilter("router") { artifact, _ ->
+//            artifact.name == "kwamp-router-core"
+//        }
+//        installer.addFilter("client") { artifact, _ ->
+//            artifact.name == "kwamp-client-core"
+//        }
+//    }
+//}
+
+//install {
+//    repositories.mavenInstaller {
+//        addFilter('api') { artifact, file -> artifact.name.endsWith('api') }
+//        addFilter('impl') { artifact, file -> artifact.name.endsWith('impl') }
+//    }
+//}
+
+//tasks.getByName<Upload>("uploadArchives") {
+//    repositories.withGroovyBuilder {
+//        "mavenDeployer" {
+//            "repository"("url" to "file://localhost/tmp/myRepo/")
+//        }
+//    }
+//}
